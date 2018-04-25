35fe8ff40cf9840d124c836efe057f31d4da331c
Andrew Zhou <ajzhou@princeton.edu>
2018-04-25 17:45:37 -0400

    highlight rectangles

--- Postgame/Postgame/ViewController/ViewController.swift
+++ Postgame/Postgame/ViewController/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 import RxSwift
 import RxCocoa
 import CoreLocation
 import AWSCognitoIdentityProvider
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate{
     fileprivate let disposeBag = DisposeBag()
     fileprivate let trackingConfiguration: ARWorldTrackingConfiguration = {
         let config = ARWorldTrackingConfiguration()
@@ -22,7 +23,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return config
     }()
 
-    // MARK:- UI Elements
+    // UI Elements
     let sceneView = ARSCNView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
     let screenshotButton = UIButton()
     let createButton = UIButton()
@@ -41,7 +42,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // AWS Cognito
     var user:AWSCognitoIdentityUser?
     var userAttributes:[AWSCognitoIdentityProviderAttributeType]?
-    var descriptorCache = [String: [UInt8]]()
+    private var descriptorCache = [String: [UInt8]]()
+    
+    // Poster Rx
+    private let frameSubject = PublishSubject<ARFrame>()
+    private let longPressSubject = PublishSubject<UILongPressGestureRecognizer>()
+    private var highlightedRectangleOutlineLayers = [CAShapeLayer]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,7 +55,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Setup buttons design on the main screen
         setupUILayout()
         
-        // Setup Rx functions
+        // Setup Button Rx functions
         setupScreenshoButtonRx()
         setupResetButtonRx()
         setupCreateButtonRx()
@@ -61,6 +67,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        setupAWSS3Service()
 //        let key = "40.3496/-74.6574/2018-04-09@10:28:26/surface"
 //        s3.download(for: key)
+        
+        // Setup AR Poster Discovery and Placement Rx
+        setupARPosetrDiscoveryAndPlacementRx()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -68,6 +77,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
         // Run the view's session
         sceneView.session.run(trackingConfiguration)
+        sceneView.session.delegate = self
         
         // AWS Cognito
         fetchUserAttributes()
@@ -80,12 +90,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        sceneView.session.run(trackingConfiguration, options: .removeExistingAnchors)
-    }
-    
     
     /**
         Hide ViewDidLoad UIButtons
@@ -100,7 +104,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 }
 
 /**
- Rx for ViewDidLoad Buttons
+ MARK:- Rx for ViewDidLoad Buttons
  */
 extension ViewController {
     /**
@@ -192,7 +196,7 @@ extension ViewController {
 
 
 /**
- Location Service.
+ MARK:- Location Service.
  */
 extension ViewController {
     // Note on GPS coordinates:
@@ -210,14 +214,13 @@ extension ViewController {
         
         // Get the current user coordinate
         let userLocation = geolocationService.location
-            .debug("Before map1")
-            .map{
-//                test -> (Double, Double) in
+            .debug("Descriptor Cache: Before map1")
+            .map{ // test -> (Double, Double) in
                 // Convert CLLocation coordinates to 4 decimal places
                 (self.roundToDecimal4($0.latitude as Double), self.roundToDecimal4($0.longitude as Double))
 //                return (40.3496, -74.6574)
             }
-            .debug("After map1")
+            .debug("Descriptor Cache: After map1")
             .distinctUntilChanged({ (location1, location2) -> Bool in
                 // Only send request if output changed
                 let precision = 0.0002
@@ -245,12 +248,12 @@ extension ViewController {
                 // Get surrounding coordinates
                 return self.surroundingCoordinates(for: coordinate)
             }
-            .debug("After map2")
+            .debug("Descriptor Cache: After map2")
             .flatMap { locations in
                 // Get list or URLs asscossiated with coordinates
                 return self.s3.urlList(for: locations)
             }
-            .debug("After flat-map")
+            .debug("Descriptor Cache: After flat-map")
             .flatMap({ (url) in
                 // Download descriptors
                 return self.s3.downloadDescriptor(url)
@@ -309,7 +312,7 @@ extension ViewController {
 }
 
 /**
- AWS S3.
+ MARK:- AWS S3.
  */
 extension ViewController {
 //    func setupAWSS3Service() {
@@ -325,7 +328,7 @@ extension ViewController {
 }
 
 /**
- AWS Cognito.
+ MARK:- AWS Cognito.
  */
 extension ViewController {
     func fetchUserAttributes() {
@@ -343,5 +346,274 @@ extension ViewController {
             })
             return nil
         })
+    }
+}
+
+/**
+ MARK:- ARSession Controls.
+ */
+extension ViewController {
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        frameSubject.onNext(frame)
+    }
+    
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        print("ARSession: Session Was Interrupted")
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        print("ARSession: Session Interruption Ended")
+        self.sceneView.session.run(self.trackingConfiguration, options: .removeExistingAnchors)
+    }
+    
+   
+}
+
+/**
+ MARK:- Setup Poster Rx, main per-frame opertaions.
+ */
+
+extension ViewController {
+    /**
+     Setup Poster Rx, main per-frame opertaions.
+     */
+    private func setupARPosetrDiscoveryAndPlacementRx() {
+        // Start observing long press gesture on view
+        setupLongPressSubject()
+        
+        // Observe ARFrame. Slow down frame rate. Remove drawn outlines for selected rectangles, if any exist
+        let frameObservable =
+            frameSubject.asObservable()
+                .debug("Poster: Before Throttle")
+                .throttle(0.1, scheduler:  MainScheduler.instance) // slow down requests
+                .debug("Poster: After Throttle")
+                .do(onNext: { (_) in
+                    self.removeRectOutlineLayers() // clean up drawings, if there is any
+                })
+       
+        // Observe dectected rectangle observations for each frame, when isPosting == false
+        let notPostingRectObservable = frameObservable
+            .filter { _ -> Bool in
+                if !self.isPosting {
+                    return true
+                } else {
+                    return false
+                }
+            }
+            .debug("Poster: After Not Posting Filter")
+            .flatMap{ self.detectRectangles(in: $0) }
+            .debug("Poster: After Rectangle Detection (Not Posting)")
+    
+        // Observe dectected rectangle observations for each frame, when isPosting == true
+        let isPostingRectObservable =
+            frameObservable
+                .filter { _ -> Bool in
+                    if self.isPosting {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+                .flatMap{ self.detectRectangles(in: $0) }
+        
+        // Observe long press gestures. Filter gestures that are not on a vertical plane.
+        let longPressObservable =
+            longPressSubject.asObserver()
+                .debug("Poster-Press: Before OnPlane Filter")
+                .filter { (sender) -> Bool in
+                    // sender location must be on a plane
+                    let currTouchLocation = sender.location(in: self.sceneView)
+                    return self.isOnVerticalPlane(currTouchLocation)
+                }
+                .debug("Poster-Press: After OnPlane Filter")
+       
+        // Observe long press that have state began or changed
+        let longPressActiveObservable =
+            longPressObservable
+                .filter { (sender) -> Bool in
+                    if sender.state == .began || sender.state == .changed{
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+                .debug("Poster-Press: after Active Filter")
+        
+        // Observe long press that have state ended
+        let longPressEndedObservable =
+            longPressObservable
+                .filter { (sender) -> Bool in
+                    if sender.state == .ended {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+                .debug("Poster-Press: after Ended Filter")
+        
+        
+        // longPressActive + Not Posting --> highlight selected rectangles
+        longPressActiveObservable
+            .withLatestFrom(notPostingRectObservable) { (sender, observations) -> VNRectangleObservation? in
+                if (observations == nil) { return nil }
+                // Select rectangle observation that contains current touch location
+                let currTouchLocation = sender.location(in: self.sceneView)
+                for observation in observations! {
+                    let convertedRect = self.sceneView.convertFromCamera(observation.boundingBox)
+                    if convertedRect.contains(currTouchLocation) {
+                        return observation
+                    }
+                }
+                return nil
+            }
+            .debug("Poster: after (longPressActive + Not Posting) withLatestFrom")
+            .filter{ $0 != nil }
+            .asDriver(onErrorJustReturn: nil)
+            .drive(onNext: { (observation) in
+                // Outline selected observation
+                if let _ = observation {
+                    self.highlightObservation(observation!)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    /**
+     Observe long press gesture to facilitate user interaction with poster discovery and placement.
+     */
+    private func setupLongPressSubject() {
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(observeLongPress(sender:)))
+        view.addGestureRecognizer(longPress)
+    }
+    @objc private func observeLongPress(sender: UILongPressGestureRecognizer) {
+        longPressSubject.onNext(sender)
+    }
+    
+    /**
+     Detect rectangles in a frame.
+     */
+    private func detectRectangles(in frame: ARFrame) -> Observable<[VNRectangleObservation]?>{
+        return Observable.create({ observer in
+            let request = VNDetectRectanglesRequest(completionHandler: { (request, error) in
+                // Filter observations and observe detected results
+                guard let observations = request.results as? [VNRectangleObservation],
+                    let _ = observations.first else {
+                        observer.onNext(nil)
+                        observer.onCompleted()
+                        return
+                }
+                
+                let filteredObservations = self.filterContainedRects(observations)
+                observer.onNext(filteredObservations)
+                observer.onCompleted()
+            })
+            
+            // Don't limit resulting number of observations
+            request.maximumObservations = 1
+            request.quadratureTolerance = 5
+            request.minimumConfidence   = 0.6
+            //            request.minimumAspectRatio  = 0.5
+            //            request.maximumAspectRatio  = 2.0
+            
+            // Perform request
+            let handler = VNImageRequestHandler(cvPixelBuffer: frame.capturedImage, options: [:])
+            try? handler.perform([request])
+            return Disposables.create()
+        })
+    }
+    
+    /**
+     Filter out rectangle observations that are contained by another rectangle observation.
+     */
+    fileprivate func filterContainedRects(_ observations: [VNRectangleObservation]) -> [VNRectangleObservation]{
+        // Sort in increasing order
+        var filtered = [VNRectangleObservation]()
+        let sorted = observations.sorted(by: { (o1, o2) -> Bool in
+            if o1.boundingBox.size.smaller(than: o2.boundingBox.size) {
+                return true
+            }
+            return false
+        })
+        
+        // Check if observation is contained
+        let length = sorted.count
+        for i in 0..<length{
+            let currObservation = sorted[i]
+            
+            var contained = false
+            for j in (i+1)..<length {
+                if sorted[j].boundingBox.contains(currObservation.boundingBox) {
+                    contained = true
+                    break
+                }
+            }
+            
+            // Appened uncontained observation
+            if !contained {
+                filtered.append(currObservation)
+            }
+        }
+        return filtered
+    }
+    
+    /**
+     Outline selected rectangle observation.
+     */
+    fileprivate func highlightObservation(_ observation: VNRectangleObservation) {
+        let points = [observation.topLeft, observation.topRight, observation.bottomRight, observation.bottomLeft]
+        let convertedPoints = points.map { self.sceneView.convertFromCamera($0) }
+        let layer = drawPolygon(convertedPoints, color: .red)
+        self.highlightedRectangleOutlineLayers.append(layer)
+        self.sceneView.layer.addSublayer(layer)
+    }
+    
+    /**
+     Draw outline given set of points and color.
+     */
+    fileprivate func drawPolygon(_ points: [CGPoint], color: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.fillColor = nil
+        layer.strokeColor = color.cgColor
+        layer.lineWidth = 5
+        let path = UIBezierPath()
+        path.move(to: points.last!)
+        points.forEach { point in
+            path.addLine(to: point)
+        }
+        layer.path = path.cgPath
+        return layer
+    }
+    
+    /**
+     Remove outlines drawn for selected rectangles.
+     */
+    fileprivate func removeRectOutlineLayers() {
+        // Remove outline for observed rectangles
+        for layer in self.highlightedRectangleOutlineLayers {
+            layer.removeFromSuperlayer()
+        }
+        self.highlightedRectangleOutlineLayers.removeAll()
+    }
+    
+    
+    /**
+     Check if touch location is on a vertical plane.
+     */
+    fileprivate func isOnVerticalPlane(_ point: CGPoint) -> Bool {
+        let results = sceneView.hitTest(point, types: .existingPlaneUsingExtent)
+        if let _ = results.first{
+        return true
+        }
+        return false
     }
 }
