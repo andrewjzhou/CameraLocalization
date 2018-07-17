19d6be9c49a135fddc8a40b5d3d7cd1e0df73306
Andrew Zhou <ajzhou@princeton.edu>
2018-07-16 19:10:07 -0500

    password validation

--- Postgame/Postgame/Controller/SignUp/PasswordViewController.swift
+++ Postgame/Postgame/Controller/SignUp/PasswordViewController.swift
@@ -29,10 +29,27 @@ class PasswordViewController: SignUpBaseViewController {
     }
     
     override func buttonActionCondition() -> Bool {
-        // set input field requirements
+        if !textField.text!.isValidPassword() {
+            let alertController = UIAlertController(title: "Invalid Password.",
+                                                    message: "Minimum 8 characters\n At least 1 Lowercase Alphabet\n At least 1 Number",
+                                                    preferredStyle: .alert)
+            let retryAction = UIAlertAction(title: "Retry", style: .default, handler: nil)
+            alertController.addAction(retryAction)
+            
+            self.present(alertController, animated: true, completion:  nil)
+            return false
+        }
+        
         return true
     }
-    
-    
 
+
+}
+
+fileprivate extension String {
+    func isValidPassword() -> Bool {
+        // Minimum 8 characters at least 1 Alphabet and 1 Number
+        let passwordRegex = "^(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
+    }
 }
